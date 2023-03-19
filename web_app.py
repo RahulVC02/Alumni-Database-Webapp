bac84df07f20e353b9f50e736a5cfeaf70b08a90
Kanishk-py <kanishk.singhal@iitgn.ac.in>
2023-03-19 19:41:58 +0530

    bugzs

--- web_app.py
+++ web_app.py
@@ -256,7 +256,8 @@ def edit_insert():
         
         return render_template('tables_before_after.html', table_before=table_data_before, table_after=table_data_after,table_name=table_name,
                             table_col_names=TABLE_COLUMN_NAMES)
-    except:
+    except Exception as e:
+        print(e)
         return render_template('errors.html', errorMessage="Input Error- Re-check your input against the schema.")
 
 
