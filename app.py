69e530ba38e5da11a00c9000e86a7512a8d87a18
rahulvc02 <rahul.chembakasseril@iitgn.ac.in>
2023-04-07 15:51:52 +0530

    new.

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ Session(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'alumni.dbms.g2@gmail.com'
-app.config['MAIL_PASSWORD'] = 'uqnouuxkfueqzbvt'
+app.config['MAIL_PASSWORD'] = 'yjkxjsnfjktyzclx'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
